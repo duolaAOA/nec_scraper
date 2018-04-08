7fe51c1209d33bc3f762dde70805d7f60ed1616a
duolaAOA <1219960386@qq.com>
2018-04-08 21:55:15 +0800

    parse channel

--- nec_scraper/spiders/huxiu.py
+++ nec_scraper/spiders/huxiu.py
@@ -2,19 +2,19 @@
 import scrapy
 from scrapy_redis.spiders import RedisSpider
 
-from nec_scraper import lxml_select
+from nec_scraper import lxml_select as ls
 
 
 class HuxiuSpider(RedisSpider):
     name = "huxiu"
     redis_key = "huxiu:start_urls"
-    huxiu_baseurls = "https://www.huxiu.com/"
+    huxiu_baseurls = "https://www.huxiu.com"
     channelhref = set()
     tagsHasBeenCrawled = False
     tags = set()
 
     def parse(self, response):
-        channelhrefs = response.xpath(lxml_select.CHANNELHREFS_X).extract()
+        channelhrefs = response.xpath(ls.CHANNELHREFS_X).extract()
         for href in channelhrefs:
             href = self.huxiu_baseurls + href
             self.channelhref.add(href)
@@ -22,4 +22,32 @@ class HuxiuSpider(RedisSpider):
             yield scrapy.Request(url=href, callback=self.parse_channel)
 
     def parse_channel(self, response):
+        """
+        处理每个分类栏目
+        :param response:   channelhrefs
+        :return:
+        """
+        topArticles=[]
+
+        hotArticles=[]
+
+        specials=[]
+
+        authors=[]
+
+        try:
+            topArticlesHrefs = response.xpath(ls.topArticlesHrefs_X)
+            for href in topArticlesHrefs:
+                topArticles.append(self.huxiu_baseurls + href)
+            try:
+                for href in topArticles:
+                    yield scrapy.Request(url=href, callback=self.parse_article)
+
+            except Exception as e:
+                self.logger.info('topArticlesHrefs faield:  ' + repr(str(e)))
+
+        except Exception as e:
+            self.logger.info('topArticles faield:  ' + repr(str(e)))
+
+    def parse_article(self, response):
         pass
