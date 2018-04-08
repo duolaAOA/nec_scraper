ce4f67cecb3781ee67394f93d54d622528db3fe6
hetii <1219960386@qq.com>
2018-04-08 21:53:39 +0800

    add topArticles xpath

--- nec_scraper/lxml_select.py
+++ nec_scraper/lxml_select.py
@@ -2,4 +2,7 @@
 """
 xpath or css   解析
 """
+
 CHANNELHREFS_X =  '//div[@class="container"]/ul[contains(@class,"navbar-left")]/li[contains(@class,"js-show-menu")]/ul/li/a/@href'
+
+topArticlesHrefs_X = '//div[@class="container"]/div[contains(@class,"wrap-left")]/div[@class="mod-info-flow"]/div[contains(@class,"mod-art")]/div[contains(@class,"mob-ctt")]/h2/a[contains(@class,"transition")]/@href'
